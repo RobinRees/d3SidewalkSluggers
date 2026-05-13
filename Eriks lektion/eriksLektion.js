39bac3110db5df42397aab2f4f15f5c22ffd9a24
swesslen <simon@jolowe.se>
2026-05-13 15:45:47 +0200

    reduce

--- Eriks lektion/eriksLektion.js
+++ Eriks lektion/eriksLektion.js
@@ -77,3 +77,57 @@ for (let discipline of disciplines) {
 }
 
 disciplines.forEach()
+
+
+
+
+
+// reduce REDUCE reduce REDUCE reduce REDUCE reduce REDUCE reduce REDUCE reduce REDUCE reduce REDUCE reduce REDUCE reduce REDUCE
+
+{
+    [1, 2, 3].reduce((acc, curr) => {
+        return acc + curr;
+    }, 0); // 0 är startvärdet, initialValue, acc's första iterationen
+}
+
+// första iterationen kommer acc vara 0, och curr att vara 1 (första elementet i arrayen);
+// de andra iterationerna kommer acc att vara det som returnerades föregående;
+
+// 1: acc(0) + curr(1) = 1
+// 2: acc(1) + curr(2) = 3
+// 3: acc(3) + curr(3) = 6
+
+// Alternativt
+{
+    const initialValue = 0;
+
+    [1, 2, 3].reduce((acc, curr) => {
+        return acc + curr;
+    }, initialValue); // same same men kan reglera startvärdet
+}
+
+// ------------------------- Test ----------------------------
+
+const sumOfAllIds = disciplines.reduce((acc, curr) => {
+    const idCurrent = curr.id;
+    return acc + idCurrent;
+}, 0);
+
+
+
+const sumOfAllFactors = disciplines.reduce((acc, curr) => {
+    let sumFactors = 0;
+    for (let key in curr.skillFactors) sumFactors += curr.skillFactors[key];
+    return acc + sumFactors;
+}, 0)
+
+// ---------------------------------------------------------------
+
+// A = s1: 17, s2: 16, s3: 19
+// B = s1: 21, s2: 13, s3: 15
+
+// A = a1: 361, a2 401, b1: 120, b2: 50
+// B = a1: 250, a2: 173, b1: 150, b2: 401
+
+//s1 för A.
+// (Pa1 + Pa2) * As1 + (Pb1 + Pb2) * Bs1 = 
