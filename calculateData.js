bceed5496feffa1e129448237362b9fbaa0f7739
RobinRees <Reehees12@gmail.com>
2026-05-15 14:10:24 +0200

    totalScorePerSeasons

--- calculateData.js
+++ calculateData.js
@@ -35,3 +35,44 @@ function calculateStats () {
 
 
 */
+
+
+function totalScorePerSeason () {
+    participants.forEach(participant => {
+        participant.totalScorePerSeason = {
+            year0: 0,
+            year1: 0,
+            year2: 0,
+            year3: 0,
+            year4: 0,
+            year5: 0,
+            year6: 0,
+            year7: 0,
+            year8: 0,
+            year9: 0
+        }
+    })
+
+    seasons.forEach(season => {
+        let currentYear = season.year;
+
+        season.competitionDays.forEach(day => {
+            day.events.forEach(event => {
+                event.scores.forEach(score => {
+                    participants.forEach(person => {
+                        if (person.id === score.participantId) {
+                            person.totalScorePerSeason[`year${currentYear}`] += score.score;
+                        }
+                    })
+                })
+            })
+        })
+    })
+
+}
+
+totalScorePerSeason();
+
+function disciplineScorePerSeason () {
+
+}
