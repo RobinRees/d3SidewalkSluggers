bce268eb425185b6a9dbb5ae366e356647826c7e
yehorgorstein <gorstein.yehor@gmail.com>
2026-05-24 02:43:50 +0200

    fhhfh

---
+++ JS/calculateDataYehor.js
@@ -0,0 +1,19 @@
+function calculateStats() {
+    participants.forEach(participant => {
+         participant.stats = {
+            S01: 0,
+            S02: 0,
+            S03: 0,
+            S04: 0,
+            S05: 0
+        }
+    });
+
+    seasons.forEach(season => {
+        season.competitionDays.forEach(competitionDay => {
+            competitionDay.events.forEach(event => {
+                const discipline 
+            })
+        })
+    })
+}
